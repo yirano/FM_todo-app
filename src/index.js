8d2f3d83bf49b1c35ea95893d3cc6386e1c47bcb
yirano <yirano@protonmail.ch>
2020-11-28 11:58:47 -0500

    Set up project with parcel-bundler and organize project structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { render } from "react-dom"
+import "./Stylesheets/css/index.css"
+import App from "./Components/App"
+
+render(<App />, document.querySelector("#root"))
