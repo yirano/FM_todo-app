5a8378113240ebec64cd810f12dd406701548c65
yirano <yirano@protonmail.ch>
2020-11-28 16:28:30 -0500

    Set up light/dark mode in Scss

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './Form'
 import TodoList from './Todo/TodoList'
 import Header from './Header'
 
 const App = () => {
+    const [mode, setMode] = useState("light")
+
     return (
-        <div className="app">
+        <div className={`app ${mode}`}>
+            <div className="background" />
             <div className="container">
                 <Header />
                 <div className="todo">
